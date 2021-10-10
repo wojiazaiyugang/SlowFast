f8419e3c3d090d360b82fe6a0559eb0c563b19f0
wojiazaiyugang <wojiazaiyugang@outlook.com>
2021-10-10 23:01:16 +0800

    更新docker命令

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 
 import cv2
-file = Path(r"/workspace/SlowFast/demo/AVA/videoplayback.mp4")
-print(file.exists())
-video = cv2.VideoCapture(str(file))
-print(video.isOpened())
+# file = Path(r"/workspace/SlowFast/demo/AVA/videoplayback.mp4")
+# print(file.exists())
+# video = cv2.VideoCapture(str(file))
+# print(video.isOpened())
+#
+# exit()
 
-exit()
 
-
-dataset = Path(r"/home/senseport0/Workspace/SlowFast/data/KTH")
+dataset = Path(r"/workspace/SlowFast/data/KTH")
 with open(dataset.joinpath("train.csv"), "w") as train, \
         open(dataset.joinpath("val.csv"), "w") as val:
-    for class_index, class_name in enumerate(["running", "walking"]):
+    for class_index, class_name in enumerate(["running", "walking", "handclapping"]):
         videos = list(dataset.joinpath(class_name).iterdir())
         for video_index, video in enumerate(videos):
             if video_index < 0.8 * len(videos):
